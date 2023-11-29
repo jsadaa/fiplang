00a9a7a876379c38233f5ef733fd711408f67828
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-11-29 20:14:29 +0100

    refactor main

--- Program.cs
+++ Program.cs
@@ -9,75 +9,47 @@ namespace FipLang
     {
         private static void Main(string[] args)
         {
+            // Initialize data repository to store variables
+            Repository dataRepository = new Repository();
+            string? input;
+
+            // If no arguments are passed, start interactive mode
             if (args.Length == 0)
             {
                 Console.WriteLine("Fip 1.0.0");
-                // usage is fip with an optional filename argument
                 Console.WriteLine("Usage: fip [filename]");
                 Console.WriteLine("Press Ctrl+C to exit");
                 Console.WriteLine();
                 
-                // create the repository to store the data
-                Repository dataRepository = new Repository();
-                
                 while (true)
                 {
                     Console.Write("fip> ");
-                    string? input = Console.ReadLine();
-                
-                    try
-                    {
-                        // create the lexer, parser
-                        ICharStream inputStream = CharStreams.fromString(input);
-                        FipLexer fipLexer = new FipLexer(inputStream);
-                        CommonTokenStream commonTokenStream = new CommonTokenStream(fipLexer);
-                        FipParser fipParser = new FipParser(commonTokenStream);
-                        
-
-                        // parse the file and create the visitor
-                        FipParser.FileContext fileContext = fipParser.file();
-                        CustomFipVisitor visitor = new CustomFipVisitor(dataRepository);
-                
-                        var resultContent = new StringBuilder();
-
-                        // visit each context
-                        foreach (var commandline in fileContext.commandline())
-                        {
-                            var data = visitor.Visit(commandline);
-                    
-                            // if the commandline data return is not void, append the data to the result content
-                            if (data.Type != Integrated.Void)
-                                resultContent.AppendLine(data.Value.ToString());
-                        }
-
-                        Console.Write(resultContent);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                        return;
-                    }
+                    input = Console.ReadLine();
+                    if (input == null) continue;
+                    InterpretAndExecute(input, dataRepository);
                 }
             }
             
+            // If a filename is passed, execute the file
+            input = File.ReadAllText(args[0]);
+            InterpretAndExecute(input, dataRepository);
+          
+        }
+
+        private static void InterpretAndExecute(string input, Repository dataRepository)
+        {
             try
             {
-                // read the file
-                string input = File.ReadAllText(args[0]);
-
                 // create the lexer, parser
                 ICharStream inputStream = CharStreams.fromString(input);
                 FipLexer fipLexer = new FipLexer(inputStream);
                 CommonTokenStream commonTokenStream = new CommonTokenStream(fipLexer);
                 FipParser fipParser = new FipParser(commonTokenStream);
-                
-                // create the repository to store the data
-                Repository dataRepository = new Repository();
 
                 // parse the file and create the visitor
                 FipParser.FileContext fileContext = fipParser.file();
                 CustomFipVisitor visitor = new CustomFipVisitor(dataRepository);
-                
+
                 var resultContent = new StringBuilder();
 
                 // visit each context
@@ -90,7 +62,7 @@ namespace FipLang
                         resultContent.AppendLine(data.Value.ToString());
                 }
 
-                Console.WriteLine(resultContent);
+                Console.Write(resultContent);
             }
             catch (Exception e)
             {
