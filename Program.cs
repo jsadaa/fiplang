3e11291f56eb78da5ad4298a8698a01bf1e3666d
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-11-29 14:37:47 +0100

    comment main

--- Program.cs
+++ Program.cs
@@ -11,22 +11,30 @@ namespace FipLang
         {
             try
             {
+                // read the file
                 string input = File.ReadAllText("./test.fip");
 
+                // create the lexer, parser
                 ICharStream inputStream = CharStreams.fromString(input);
                 FipLexer fipLexer = new FipLexer(inputStream);
                 CommonTokenStream commonTokenStream = new CommonTokenStream(fipLexer);
                 FipParser fipParser = new FipParser(commonTokenStream);
-
-                FipParser.FileContext fileContext = fipParser.file();
+                
+                // create the repository to store the data
                 Repository dataRepository = new Repository();
-                CustomFipVisitor visitor = new CustomFipVisitor(dataRepository);
 
+                // parse the file and create the visitor
+                FipParser.FileContext fileContext = fipParser.file();
+                CustomFipVisitor visitor = new CustomFipVisitor(dataRepository);
+                
                 var resultContent = new StringBuilder();
 
+                // visit each commandline and get the data
                 foreach (var commandline in fileContext.commandline())
                 {
                     var data = visitor.Visit(commandline);
+                    
+                    // if the commandline is not void, append the data to the result content
                     if (data.Type != Integrated.Void)
                         resultContent.AppendLine(data.Value.ToString());
                 }
